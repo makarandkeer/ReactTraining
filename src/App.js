b462e5209fd5920bcc4238aa06e6a0343ac823a2
keer <keer@MacBook-Pro.local>
2021-04-04 12:46:27 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+import axios from "axios";
+
+const { useState } = React;
+
+const getApiData = (id) => {
+  return axios.get(`https://jsonplaceholder.typicode.com/photos?id=${id}`);
+};
 
 function App() {
+  const [apiOutput, setApiOutput] = useState("");
+  const [userPhoto, setUserPhoto] = useState();
+  const [counter, setCounter] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={logo} className="App-logo" alt="logo" />
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+          setApiOutput("Rendering data.......");
+          getApiData(counter).then(({ data }) => {
+            setApiOutput(JSON.stringify(data));
+            setUserPhoto(data[0].thumbnailUrl);
+          });
+        }}
+      >
+        Get Next Image
+      </button>
+
+      <div>
+        <p>{apiOutput}</p>
+        <p>{userPhoto}</p>
+      </div>
+      <img src={userPhoto} alt=""></img>
     </div>
   );
 }
